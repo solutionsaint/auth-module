0d9c7b90528e76f205a93c30723e5eae257a22d4
BharatKumar <bk9735732777@gmail.com>
2024-08-08 21:07:08 +0530

    blueprint basic Completed

--- app/src/main/java/com/techlambda/onlineeducation/customcanvas/CustomCanvas.kt
+++ app/src/main/java/com/techlambda/onlineeducation/customcanvas/CustomCanvas.kt
@@ -1,8 +1,12 @@
 package com.techlambda.onlineeducation.customcanvas
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,18 +14,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LineWeight
 import androidx.compose.material.icons.filled.Rectangle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -30,8 +43,10 @@ import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -39,24 +54,38 @@ import kotlin.io.path.Path
 import kotlin.io.path.moveTo
 
 data class Shape(
+    val id: Int = 0,
     val type: ShapeType,
-    val offset: com.techlambda.onlineeducation.customcanvas.Offset,
+    val offset: Coordinates,
     val width: Float,
     val height: Float,
     val rotationDegree: Float,
+    val color: Color = Color.Black
 )
 
-data class Offset(
+fun Shape.toDpSize(): DpSize {
+    return DpSize(width.dp, height.dp)
+}
+
+data class Coordinates(
     val x: Float,
     val y: Float
 )
 
-fun com.techlambda.onlineeducation.customcanvas.Offset.toComposeOffset(): Offset {
+fun Coordinates.toComposeOffset(): Offset {
     return Offset(this.x, this.y)
 }
 
-fun com.techlambda.onlineeducation.customcanvas.Offset.toIntOffset(): IntOffset {
+fun Coordinates.toIntOffset(): IntOffset {
     return IntOffset(this.x.toInt(), this.y.toInt())
+}
+
+fun IntOffset.toComposeOffset(): Offset {
+    return Offset(this.x.toFloat(), this.y.toFloat())
+}
+
+fun Offset.toCoordinates(): Coordinates {
+    return Coordinates(this.x, this.y)
 }
 
 enum class ShapeType {
@@ -66,30 +95,93 @@ enum class ShapeType {
     STAIR,
 }
 
+val TAG = "BlueprintDrawer"
 
 @Composable
 fun BlueprintDrawer(modifier: Modifier = Modifier.fillMaxSize()) {
     val shapesList = remember {
-        mutableListOf<Shape>()
+        mutableStateListOf<Shape>()
     }
-    var selectedShape by remember(key1 = shapesList) {
-        mutableStateOf(shapesList.lastOrNull())
+    var selectedShape by remember {
+        mutableStateOf<Shape?>(null)
     }
+
+    var offset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+    var height by remember {
+        mutableStateOf(100f)
+    }
+    var width by remember {
+        mutableStateOf(100f)
+    }
+    var rotation by remember {
+        mutableFloatStateOf(0f)
+    }
+
+    LaunchedEffect(shapesList.size) {
+        selectedShape = shapesList.lastOrNull()
+        Log.d(TAG, "offset: size $selectedShape")
+    }
+
+    LaunchedEffect(key1 = offset, key2 = height, key3 = width) {
+        selectedShape =
+            selectedShape?.copy(offset = offset.toCoordinates(), height = height, width = width)
+        Log.d(TAG, "offset: " + selectedShape?.offset)
+        Log.d(TAG, "offset: " + offset)
+        shapesList.replaceAll {
+            if (it.id == selectedShape?.id) {
+                selectedShape!!
+            } else {
+                it
+            }
+        }
+    }
+    LaunchedEffect(key1 = rotation) {
+        selectedShape = selectedShape?.copy(rotationDegree = rotation)
+        Log.d(TAG, "offset: " + selectedShape?.offset)
+        Log.d(TAG, "offset: " + offset)
+        shapesList.replaceAll {
+            if (it.id == selectedShape?.id) {
+                selectedShape!!
+            } else {
+                it
+            }
+        }
+    }
+
     Column(modifier = Modifier.fillMaxSize()) {
         Row(modifier = Modifier.fillMaxWidth()) {
             IconButton(onClick = {
                 val shape = Shape(
+                    id = shapesList.size,
                     type = ShapeType.ROOM,
-                    offset = com.techlambda.onlineeducation.customcanvas.Offset(100f, 100f),
+                    offset = Coordinates(0f, 0f),
+                    width = 100f,
+                    height = 100f,
+                    rotationDegree = 0f,
+                )
+                offset = shape.offset.toComposeOffset()
+                height = shape.height
+                width = shape.width
+                rotation = shape.rotationDegree
+                shapesList.add(shape)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Rectangle,
+                    contentDescription = "room",
+                    tint = Color.Black
+                )
+            }
+            IconButton(onClick = {
+                val shape = Shape(
+                    type = ShapeType.DOOR,
+                    offset = Coordinates(0f, 0f),
                     width = 100f,
                     height = 100f,
                     rotationDegree = 0f,
                 )
                 shapesList.add(shape)
-            }) {
-                Icon(imageVector = Icons.Default.Rectangle, contentDescription = "room")
-            }
-            IconButton(onClick = {
             }) {
                 Icon(imageVector = Icons.Default.LineWeight, contentDescription = "line")
             }
@@ -100,46 +192,80 @@ fun BlueprintDrawer(modifier: Modifier = Modifier.fillMaxSize()) {
 
 
         }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text("Width: ${width.toInt()}")
+            Slider(
+                value = width,
+                onValueChange = {
+                    width = it
+                },
+                valueRange = 50f..300f, // Adjust the range as needed
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        // Height Slider
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text("Height: ${height.toInt()}")
+            Slider(
+                value = height,
+                onValueChange = {
+                    height = it
+                },
+                valueRange = 50f..300f, // Adjust the range as needed
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        // Rotation Slider
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text("Rotation: ${rotation.toInt()}°")
+            Slider(
+                value = rotation,
+                onValueChange = {
+                    rotation = it
+                },
+                valueRange = 0f..360f, // 0 to 360 degrees
+                modifier = Modifier.weight(1f)
+            )
+        }
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(20.dp)
                 .border(width = 4.dp, color = Color.Black)
         ) {
-            val tempShapeList = shapesList.toMutableList()
-            tempShapeList.removeIf { it == selectedShape }
-            tempShapeList.forEach { shape ->
-                Canvas(
-                    modifier = Modifier
-                        .offset {
-                            shape.offset.toIntOffset()
-                        }
-                ) {
-                    drawShape(shape)
-                }
-            }
+            shapesList.forEach {
+                if (it.id != selectedShape?.id) {
+                    Canvas(
+                        modifier = Modifier
+                            .size(size = it.toDpSize())
+                            .offset {
+                                IntOffset(it.offset.x.toInt(), it.offset.y.toInt())
+                            }
 
+                    ) {
+                        drawShape(it)
+                    }
+                }
+
+            }
             selectedShape?.let {
                 Canvas(
                     modifier = Modifier
-                        .pointerInput(Unit) {
-                            detectDragGestures(
-                                onDragStart = {
-                                },
-                                onDrag = { _, dragAmount ->
-                                    selectedShape = selectedShape?.copy(
-                                        offset = com.techlambda.onlineeducation.customcanvas.Offset(
-                                            it.offset.x + dragAmount.x,
-                                            it.offset.y + dragAmount.y
-                                        )
-                                    )
-                                },
-                                onDragCancel = {
-                                },
-                                onDragEnd = {
-                                }
-                            )
+                        .size(size = it.toDpSize())
+                        .offset {
+                            IntOffset(offset.x.toInt(), offset.y.toInt())
                         }
+                        .pointerInput(Unit) {
+                            detectDragGestures { change, dragAmount ->
+                                offset = offset.plus(dragAmount)
+                            }
+                            detectTransformGestures { _, pan, _, _ ->
+                                //   offset = offset.plus(pan)
+                            }
+                        }
+
                 ) {
                     drawShape(it)
                 }
@@ -149,12 +275,12 @@ fun BlueprintDrawer(modifier: Modifier = Modifier.fillMaxSize()) {
 }
 
 fun DrawScope.drawShape(shape: Shape) {
-    rotate(shape.rotationDegree, pivot = shape.offset.toComposeOffset()) {
+    rotate(shape.rotationDegree) {
         when (shape.type) {
             ShapeType.ROOM -> {
                 drawRect(
-                    color = Color.Blue,
-                    size = Size(shape.width, shape.height),
+                    color = shape.color,
+                    size = Size(size.width, size.height),
                     style = Stroke(
                         width = 4.dp.toPx(),
                     )
@@ -164,7 +290,6 @@ fun DrawScope.drawShape(shape: Shape) {
             ShapeType.DOOR -> {
                 drawRect(
                     color = Color.Red,
-                    topLeft = shape.offset.toComposeOffset(),
                     size = Size(shape.width, shape.height)
                 )
             }
