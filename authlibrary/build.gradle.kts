0421f2b258a5fe646a6fb7d188c04b840e708c36
AdarshDalai <adarshkumar.dalai@gmail.com>
2024-08-24 18:42:29 +0530

    Removed some dependencies

--- authlibrary/build.gradle.kts
+++ authlibrary/build.gradle.kts
@@ -58,15 +58,8 @@ dependencies {
     implementation(libs.androidx.material3.icons)
     implementation(libs.androidx.material3.icons.extended)
     implementation(libs.androidx.navigation.compose)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
-
+    
     //Hilt
     implementation(libs.dagger.hilt)
     kapt(libs.dagger.hilt.compiler)
