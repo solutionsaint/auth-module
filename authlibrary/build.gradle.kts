67a8042447b7bd7e5de8e63bd0d3b5955eb23da1
AdarshDalai <adarshkumar.dalai@gmail.com>
2024-08-26 00:10:01 +0530

    Library test2

--- authlibrary/build.gradle.kts
+++ authlibrary/build.gradle.kts
@@ -36,6 +36,17 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
