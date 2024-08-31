b58fb65c2653ce04245532f28888d5636b280ece
BharatKumar <bk9735732777@gmail.com>
2024-08-31 11:55:31 +0530

    Auth Response Change

--- authlibrary/src/main/java/com/techlambda/authlibrary/ui/models/SignUpResponse.kt
+++ authlibrary/src/main/java/com/techlambda/authlibrary/ui/models/SignUpResponse.kt
@@ -1,7 +1,9 @@
 package com.techlambda.authlibrary.ui.models
 
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class SignUpResponse(
     @SerializedName("name") val name: String,
     @SerializedName("username") val username: String,
@@ -10,7 +12,7 @@ data class SignUpResponse(
     @SerializedName("password") val password: String,
     @SerializedName("userType") val userType: String,
     @SerializedName("isDeleted") val isDeleted: Boolean,
-    @SerializedName("instituteList") val instituteList: List<Any>,
+   // @SerializedName("instituteList") val instituteList: List<S>,
     @SerializedName("_id") val id: String,
     @SerializedName("createdAt") val createdAt: String,
     @SerializedName("updatedAt") val updatedAt: String,
